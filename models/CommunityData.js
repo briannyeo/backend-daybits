ba85349516e44f6949a42aa780d11e6ec89d5044
brianyeo <brianyeo1995@gmail.com>
2022-04-09 10:19:18 +0800

    linked data between schemas

--- models/CommunityData.js
+++ models/CommunityData.js
@@ -1,15 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const communitySchema = mongoose.Schema({
-  habit: { type: String, required: true },
-  goal: { type: String, required: true },
-  startDate: { type: Date, required: true },
-  targetPerDay: { type: String, required: true },
-  daysSucceeded: { type: Number },
-  daysFailed: { type: Number },
+	journalTitle: { type: Schema.Types.ObjectId, ref: 'UserData' },
+	commenterUserId: { type: String, required: true },
+	commenterComment: { type: String, required: true },
+	commenterDate: { type: Date, required: true },
 });
 
-const CommunityData = mongoose.model("CommunityData", communitySchema);
+const CommunityData = mongoose.model('CommunityData', communitySchema);
 
 module.exports = CommunityData;
--- models/UserData.js
+++ models/UserData.js
@@ -1,15 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userDataSchema = mongoose.Schema({
-  journalEntry: { type: String, required: true },
-  journalDate: { type: Date, required: true },
-  commenterUserId: { type: String, required: true },
-  commenterComment: { type: String, required: true },
-  commenterDate: { type: Date, required: true },
-  journalLikes: { type: Number },
+	_id: Schema.Types.ObjectId,
+	user: { type: Schema.Types.ObjectId, ref: 'UserAccount' },
+	habit: { type: String, required: true },
+	goal: { type: String, required: true },
+	startDate: { type: Date, required: true },
+	targetPerDay: { type: String, required: true },
+	daysSucceeded: [{ type: Boolean }],
+	journalTitle: { type: String, required: true },
+	journalEntry: { type: String, required: true },
+	journalDate: { type: Date, required: true },
+
+	//journalLikes: { type: Schema.Types.ObjectId, ref: 'UserAccount' },
 });
 
-const UserData = mongoose.model("UserData", userDataSchema);
+const UserData = mongoose.model('UserData', userDataSchema);
 
 module.exports = UserData;
