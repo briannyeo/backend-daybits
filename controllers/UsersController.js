4fafd46eadc66e872b94a735692723242f081225
brianyeo <brianyeo1995@gmail.com>
2022-04-14 22:19:22 +0800

    added sessions to userControllers

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -24,7 +24,7 @@ users.get('/seedaccount', async (req, res) => {
 	}
 });
 
-//Index route
+//Index route. Getting the data from the front-end when user registers.
 users.get('/', (req, res) => {
 	UserAccount.find()
 		.then((userInfo) => {
@@ -35,6 +35,7 @@ users.get('/', (req, res) => {
 		});
 });
 
+//Adds new users accounts to MongoDB.
 users.post('/', async (req, res) => {
 	//overwrite the user password with the hashed password, then pass that in to our database
 	req.body.password = bcrypt.hashSync(
@@ -48,6 +49,41 @@ users.post('/', async (req, res) => {
 	} catch (error) {
 		console.log(error);
 	}
+});
+
+//SESSIONS
+users.get('/progress', (req, res) => {
+	const user = req.session.user;
+
+	if (user) {
+		res.send(user);
+	} else {
+		res.send('no entry');
+	}
+});
+
+users.post('/home', async (req, res) => {
+	const { username, password } = req.body;
+	// const hashPassword = bcrypt.hashSync(password, saltRounds);
+	const user = await UserAccount.findOne({ username });
+
+	if (!user) {
+		res.send('User not found');
+	} else if (bcrypt.compareSync(password, user.password)) {
+		req.session.user = user;
+		//req.session.count = 1;
+		res.send('Ok');
+		console.log('Successfully authenticated');
+	} else {
+		res.send('No');
+	}
+	// res.send(user);
+	//* success or failure
+});
+
+users.post('/logout', (req, res) => {
+	req.session.destroy();
+	res.send('logout');
 });
 
 module.exports = users;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const journalController = require('./controllers/journalController');
-const userController = require('./controllers/UsersController');
+const usersController = require('./controllers/UsersController');
 const session = require('express-session');
 
 const app = express();
@@ -25,18 +25,18 @@ mongoose.connection.once('open', () => {
 });
 
 //****************MIDDLEWARES***************//
-app.use(cors());
-app.use(express.json());
-app.use('/daybits/journal', journalController);
-app.use('/daybits/register', userController);
-
 app.use(
 	session({
 		secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+		cookie: { httpOnly: false },
 		resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
 		saveUninitialized: false, // default  more info: https://www.npmjs.com/package/express-session#resave
 	})
 );
+app.use(cors());
+app.use(express.json());
+app.use('/daybits/journal', journalController);
+app.use('/daybits/register', usersController);
 
 app.get('/', (req, res) => {
 	res.send('Hi 2');
